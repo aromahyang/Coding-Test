6990e2504f38c632a8543e98aa9ff2212c380549
aromahyang <haeng1605@gmail.com>
2020-06-06 00:29:19 +0900

    Add files via upload

---
+++ 정렬_가장큰수.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> String {
+    let answer = numbers.map({ String($0) })
+        .sorted(by: { Int($0+$1)! > Int($1+$0)! })
+        .joined(separator: "")
+    return answer
+}
