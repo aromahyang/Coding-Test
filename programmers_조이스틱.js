41ea93349694453b72e388a08361153bfa8750fc
aromahyang <haeng1605@gmail.com>
2021-09-02 01:23:49 +0900

    Create programmers_조이스틱.js

---
+++ programmers_조이스틱.js
@@ -0,0 +1,32 @@
+function solution(name) {
+    // check all characters are 'A'
+    if (name.split('').every((a) => a === 'A')) {
+        return 0;
+    }
+    
+    let answer = 0;
+    let dist = name.length - 1;
+    for (let i = 0; i < name.length; i++) {
+        if (name[i] !== 'A') {
+            answer += getUpDownCount(name, i);
+        } else {
+            let idx = i + 1;
+            while (idx < name.length && name[idx] === 'A') {
+                idx += 1;
+            }
+            
+            let temp = (i-1) * 2 + name.length - idx;
+            dist = Math.min(dist, temp);
+        }
+    }
+    
+    
+    return answer + dist;
+}
+
+function getUpDownCount(name, idx) {
+    let upCount = Math.abs(name.charCodeAt(idx) - 'A'.charCodeAt(0)); // count from changing character by up key
+    let downCount = Math.abs(name.charCodeAt(idx) - 'Z'.charCodeAt(0)) + 1; // count from changing character by down key
+    let diff = Math.min(upCount, downCount);
+    return diff;
+}
