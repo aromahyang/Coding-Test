0fd9beab8453dc37d774df4586dfd6ba75c83677
Aromahyang <haeng1605@gmail.com>
2024-07-27 17:01:58 +0000

    sorting 추가

---
+++ codility/lessons/sorting_Distinct.js
@@ -0,0 +1,7 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+    // Implement your solution here
+    return new Set(A).size;
+}
